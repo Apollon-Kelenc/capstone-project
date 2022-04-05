2781b395dafbd4c3de79bf93dbcab4753c11a04b
Apollon-Kelenc <domenkelenc24@gmail.com>
2022-04-05 11:35:36 +0200

    fix wine database

--- pages/quiz/[question].js
+++ pages/quiz/[question].js
@@ -4,12 +4,16 @@ import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import { quiz } from '../../lib/quiz';
 import header_picture from '/public/header-picture.jpeg';
+import { wine } from '../../lib/wine';
 
 const Question = () => {
   const router = useRouter();
   const { question } = router.query;
 
   const currentQuestion = quiz[question];
+  const resultWine = wine;
+
+  console.log(wine);
 
   if (!currentQuestion) {
     return <div>Question not found :(</div>;
@@ -33,7 +37,7 @@ const Question = () => {
               onClick={() => {
                 setTimeout(() => {
                   router.push(`/quiz/${answer.nextQuestion}`);
-                }, 200);
+                }, 300);
               }}
             >
               {answer.label}
@@ -70,9 +74,9 @@ const FoodButton = styled.button`
   font-size: 16px;
   box-shadow: 2px 2px 1px rgba(0, 0, 0, 0.2), -2px -2px 1px rgba(0, 0, 0, 0.2);
   color: black;
-  transition: 0s;
+  transition: 0.1s;
   :hover {
-    border: 3px solid;
+    border: 3px solid black;
     border-color: rgba(89, 199, 72, 1);
   }
 `;
@@ -82,7 +86,10 @@ const AppContainer = styled.div`
   flex-direction: column;
   align-items: center;
   height: 100vh;
-  background: rgba(94, 91, 91);
+  background-image: linear-gradient(
+    rgba(244, 194, 194, 1),
+    rgba(128, 0, 32, 1)
+  );
 `;
 
 const BackArrow = styled.button`
@@ -105,7 +112,6 @@ const StyledHeader = styled.h1`
   margin: 2rem 2rem;
   font-size: 27px;
   font-weight: 500;
-  color: white;
 `;
 
 const StyledCounter = styled.div`
